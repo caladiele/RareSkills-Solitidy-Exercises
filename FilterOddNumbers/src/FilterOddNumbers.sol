4d79eea3911f0f6c31552483abbea4c202e4a79c
Caladhiele <104708243+caladiele@users.noreply.github.com>
2024-01-16 09:30:27 +0100

    FilterOddNumbers

--- FilterOddNumbers/src/FilterOddNumbers.sol
+++ FilterOddNumbers/src/FilterOddNumbers.sol
@@ -10,11 +10,39 @@ contract FilterOddNumbers {
               you need to count the even numbers then declare an array of that size.
     */
 
-    function filterOdd(uint256[] memory _arr)
-        public
-        view
-        returns (uint256[] memory)
-    {
-        // your code here
+    function filterOdd(uint256[] memory _arr) public pure returns (uint256[] memory) {
+        /*
+            First, determine the length of the return array.
+        */
+        uint256 arrLength = 0;
+
+        for (uint i = 0; i < _arr.length; i++) {
+            if (_arr[i] % 2 == 0) {
+                arrLength++;
+            }
+        }
+        /*
+            Next, create an array with a fixed length, 
+            equal to the number of even numbers in the input array (_arr)
+        */
+        uint[] memory filteredArray = new uint[](arrLength);
+
+        /* Initialize a counter, useful to assign values to the right slot */
+        uint j;
+
+        /*
+            New for loop, for each value corresponding to the condition ->
+            assign the value to the first available slot in the return array.
+            Last, increment counter.
+        */
+        for (uint i = 0; i < _arr.length; i++) {
+            if (_arr[i] % 2 == 0 ) {
+                filteredArray[j] = _arr[i];
+                j++;
+            }
+        }
+
+        return filteredArray;
+
     }
 }
