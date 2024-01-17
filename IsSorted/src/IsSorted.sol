0db9f7bc989ac71ebd3ac716bed0e668630367cf
Caladhiele <104708243+caladiele@users.noreply.github.com>
2024-01-17 11:50:19 +0100

    isSorted

--- IsSorted/src/IsSorted.sol
+++ IsSorted/src/IsSorted.sol
@@ -6,6 +6,13 @@ contract IsSorted {
      * The goal of this exercise is to return true if the members of "arr" is sorted (in ascending order) or false if its not.
      */
     function isSorted(uint256[] calldata arr) public view returns (bool) {
-        // your code here
+        uint j = 0;
+        for (uint i = 0; i < arr.length; i++) {
+            if (arr[i] < j) {
+                return false;
+            }
+            j = arr[i];
+        }
+        return true;
     }
 }
