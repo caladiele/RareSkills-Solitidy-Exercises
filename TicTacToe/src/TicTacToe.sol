262ff37e11045ebe25f53358827a2af24f77ebd8
Caladhiele <104708243+caladiele@users.noreply.github.com>
2024-01-18 10:29:34 +0100

    TicTacToe

--- TicTacToe/src/TicTacToe.sol
+++ TicTacToe/src/TicTacToe.sol
@@ -11,6 +11,24 @@ contract TicTacToe {
     */
 
     function isWinning(uint8[3][3] memory board) public pure returns (bool) {
-        // your code here
-    }
+       
+        if (board[0][0] == 1) {
+            if ((board[0][1] == 1 && board[0][2] == 1) || (board[1][0] == 1 && board[2][0] == 1) || (board[1][1] == 1 && board[2][2] == 1)) {
+                return true;
+            }
+        } else if (board[0][1] == 1 && board[1][1] == 1 && board[2][1] == 1) {
+            return true;
+        } else if (board[0][2] == 1) {
+            if ((board[1][2] == 1 && board[2][2] == 1) || (board[1][1] == 1 && board[2][0] == 1))  {
+                return true;
+            }
+        } else if (board[1][0] == 1 && board[1][1] == 1 && board[1][2] == 1) {
+            return true;
+        } else if (board[2][0] == 1 && board[2][1] == 1 && board [2][2] == 1) {
+            return true;
+        } else {
+            return false;
+        }
+
+    } 
 }
