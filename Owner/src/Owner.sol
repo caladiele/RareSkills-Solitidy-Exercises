f436fc83103548bf566be1a4e83f1dd164c81688
Caladhiele <104708243+caladiele@users.noreply.github.com>
2024-01-20 09:16:41 +0100

    Owner

--- Owner/src/Owner.sol
+++ Owner/src/Owner.sol
@@ -17,6 +17,7 @@ contract OnlyOwner {
     }
 
     function updateMagicNumber(uint256 _number) public {
+        require(msg.sender == owner, "You don't have the rights to do this");
         magicNumber = _number;
     }
 }
