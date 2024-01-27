b07b419c6a8edd0e844ee6f6a6339cf7e531276e
Caladhiele <104708243+caladiele@users.noreply.github.com>
2024-01-27 11:07:56 +0100

    Withdraw

--- Withdraw/src/Withdraw.sol
+++ Withdraw/src/Withdraw.sol
@@ -3,7 +3,14 @@ pragma solidity ^0.8.13;
 
 contract Withdraw {
     // @notice make this contract able to receive ether from anyone and anyone can call withdraw below to withdraw all ether from it
-    function withdraw() public {
-        // your code here
+    receive() external payable {
+
+    }
+    function withdraw() public payable {
+       msg.sender.call{value: viewBalance()}(""); 
+    }
+
+    function viewBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }
