5c8c326d76799e657df4c655fc77cd849d08a691
Caladhiele <104708243+caladiele@users.noreply.github.com>
2024-01-27 11:28:29 +0100

    Basic Bank

--- BasicBank/src/BasicBank.sol
+++ BasicBank/src/BasicBank.sol
@@ -4,8 +4,21 @@ pragma solidity ^0.8.13;
 contract BasicBank {
     /// @notice deposit ether into the contract
     /// @dev it should work properly when called multiple times
-    function addEther() external payable {}
+
+    mapping(address => uint256) public balanceOf;
+ 
+    receive() external payable {}
+
+    function addEther() external payable {
+        balanceOf[msg.sender] += msg.value;
+    }
 
     /// @notice used to withdraw ether from the contract (No restriction on withdrawals)
-    function removeEther(uint256 amount) external payable {}
+    function removeEther(uint256 amount) external payable {
+
+        require(balanceOf[msg.sender] >= amount, "Nope ;)");
+        msg.sender.call{value: amount}("");
+        balanceOf[msg.sender] -= amount;
+        
+    }
 }
