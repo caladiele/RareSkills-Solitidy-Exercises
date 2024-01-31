165cab030a4ad35f8125b2968d2fa7f170739aa0
Caladhiele <104708243+caladiele@users.noreply.github.com>
2024-01-31 09:24:25 +0100

    Block Number

--- BlockNumber/src/BlockNumber.sol
+++ BlockNumber/src/BlockNumber.sol
@@ -10,8 +10,11 @@ contract BlockNumber {
      */
 
     address public lastCaller;
+    uint256 public lastBlockNumber;
 
     function callMe() external {
-        /// your code here
+        require(lastBlockNumber < block.number, "You have to wait more");
+        lastCaller = msg.sender;
+        lastBlockNumber = block.number;
     }
 }
